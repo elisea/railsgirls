3bc1ce0d21b487aa12a5bf19873d949a918c7690
Elise Aplin <eaplin@thoughtworks.com>
2015-07-25 17:30:54 +1000

    wildly commiting everything

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -25,7 +25,6 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-    @comment.user = current_user
 
     respond_to do |format|
       if @comment.save
