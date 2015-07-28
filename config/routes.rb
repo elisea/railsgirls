73b36b585f8f48e7f19301e46a7ee2dc7b111eb8
Elise Aplin <eaplin@thoughtworks.com>
2015-07-28 13:31:54 +1000

    attach a user name to a comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :comments
   get 'pages/info'
 
   root :to => redirect('/ideas')
   resources :ideas
+  resources :comments
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
